2b08fa7705d432b2a618cef8da81309c1074d63b
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-06 19:11:59 -0300

    navbar con logotipo y título

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+// src/components/Navbar.jsx
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img
+            src="https://i.pinimg.com/736x/fe/25/4d/fe254d40961bb7551b1472de8b579e10.jpg"
+            alt="Logo"
+            width="40"
+            className="me-2"
+          />
+          Mi Tienda
+        </a>
+      </div>
+    </nav>
+  );
+}
