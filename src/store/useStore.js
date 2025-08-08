5ebd65b4be5554aa30858e673152c13068949dd0
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-08 15:52:29 -0300

    store con Zustand para manejar carrito

---
+++ src/store/useStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+export const useStore = create((set) => ({
+  carrito: [],
+  agregarAlCarrito: (producto) =>
+    set((state) => ({
+      carrito: [...state.carrito, producto]
+    })),
+  eliminarDelCarrito: (id) =>
+    set((state) => ({
+      carrito: state.carrito.filter((p) => p.id !== id)
+    })),
+  limpiarCarrito: () => set({ carrito: [] })
+}));
