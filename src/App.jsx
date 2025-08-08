51a9c0aa7486c522629378b1710736ef4d0137ea
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-08 16:01:58 -0300

    configuración de rutas con React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Productos from './pages/Productos';
+import Carrito from './pages/Carrito';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <main className="container mt-4">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/carrito" element={<Carrito />} />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
