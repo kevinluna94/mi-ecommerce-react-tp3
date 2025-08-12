cd0ada19e6ddf8537a73e86a8ce354f949013d25
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-12 19:26:04 -0300

    Se ajustó Home para usar el hook con selector para agregar productos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { fetchDestacados } from '../api/api';
+import { useStore } from '../store/useStore';
 
 export default function Home() {
   const [productos, setProductos] = useState([]);
+  const agregarAlCarrito = useStore((state) => state.agregarAlCarrito);
 
   useEffect(() => {
     fetchDestacados().then(setProductos);
@@ -10,20 +12,20 @@ export default function Home() {
 
   return (
     <>
-    <div
+      <div
         className="hero text-black text-center p-5 mb-5"
         style={{
-        backgroundImage: 'url(https://i.pinimg.com/736x/84/68/cd/8468cdd48ba8b0e90e12051bb2a97891.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-         width: '100%',
-         height: '300px'
+          backgroundImage:
+            'url(https://i.pinimg.com/736x/84/68/cd/8468cdd48ba8b0e90e12051bb2a97891.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100%',
+          height: '300px',
         }}
->
+      >
         <h1 className="display-4 fw-bold">Bienvenido a mi Tienda</h1>
-         <p className="lead">Los mejores productos, al mejor precio</p>
-        
-    </div>
+        <p className="lead">Los mejores productos, al mejor precio</p>
+      </div>
 
       <div className="container">
         <h2 className="mb-4">Productos Destacados</h2>
@@ -31,17 +33,29 @@ export default function Home() {
           {productos.map((p) => (
             <div className="col-md-3 mb-4" key={p.id}>
               <div className="card h-100">
-                <img src={p.image} className="card-img-top p-3" style={{ height: '200px', objectFit: 'contain' }} />
+                <img
+                  src={p.image}
+                  alt={p.title}
+                  className="card-img-top p-3"
+                  style={{ height: '200px', objectFit: 'contain' }}
+                />
                 <div className="card-body d-flex flex-column">
                   <h5 className="card-title">{p.title.slice(0, 30)}</h5>
                   <p className="card-text fw-bold">${p.price}</p>
-                  <button className="btn btn-primary mt-auto">Agregar</button>
+                  <button
+                    className="btn btn-primary mt-auto"
+                    onClick={() => agregarAlCarrito(p)}
+                  >
+                    Agregar
+                  </button>
                 </div>
               </div>
             </div>
           ))}
         </div>
-        <a href="/productos" className="btn btn-outline-primary mt-3">Ver todos los productos</a>
+        <a href="/productos" className="btn btn-outline-primary mt-3">
+          Ver todos los productos
+        </a>
       </div>
     </>
   );
