fd80f8962d8ac927446ab569be1d2c2e78fabc90
kevinluna94 <kevinemanuelluna@gmail.com>
2025-08-14 15:04:30 -0300

    Cambios en los Estilos porque el codigo se superponia y no funcionaba el responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
           <img
             src="https://i.pinimg.com/736x/fe/25/4d/fe254d40961bb7551b1472de8b579e10.jpg"
             alt="Logo"
@@ -13,13 +13,28 @@ export default function Navbar() {
           />
           Mi Tienda
         </Link>
-        <div>
-          <Link to="/productos" className="btn btn-outline-light me-2">
-            Productos
-          </Link>
-          <Link to="/carrito" className="btn btn-outline-light">
-            Carrito
-          </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            <Link to="/productos" className="nav-link">
+              Productos
+            </Link>
+            <Link to="/carrito" className="nav-link">
+              Carrito
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
